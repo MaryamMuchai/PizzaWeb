9ecf3b1dbad06c0fdbe5f5bd2f0198b35dcfcd64
Marya <maryammuchai@gmail.com>
2021-03-08 00:13:46 +0300

    declaring variables

---
+++ script.js
@@ -0,0 +1,9 @@
+var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping, total ){
+    this.size = size;
+    this.crust = crust;
+  this.name = name;
+  this.topping = topping;
+  this.total = total;
+}
